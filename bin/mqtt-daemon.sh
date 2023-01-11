6a8f47dd4285eda688485afeddbaedd55267a63b
bittles <bauern.sc@gmail.com>
2023-01-11 10:07:46 -0500

    pull current mqttdaemon from cam

--- bin/mqtt-daemon.sh
+++ bin/mqtt-daemon.sh
@@ -7,7 +7,7 @@ mkdaemon() {
     #   --environ       Sets an environment variable. Used to remove buffering on stdout
     #
     # dslog options
-    #   --priority      The syslog priority. Set to DEBUG as these are just the stdout of the 
+    #   --priority      The syslog priority. Set to DEBUG as these are just the stdout of the
     #   --max-files     The number of logs that will exist at once
     #
     max_respawns=$1
@@ -26,26 +26,26 @@ mkdaemon() {
 }
 
 case "$1" in
-	start)
+        start)
         echo "#####$(basename "$0")#####"
         mkdaemon 0 mqtt-autodiscovery /media/mmc/mosquitto/bin/mqtt-autodiscovery.sh
         mkdaemon 0 mqtt-control /media/mmc/mosquitto/bin/mqtt-control.sh
         mkdaemon 0 mqtt-status /media/mmc/mosquitto/bin/mqtt-status.sh
         mkdaemon 0 mqtt-motion /media/mmc/mosquitto/bin/mqtt-motion.sh
-		;;
+                ;;
     stop)
         echo "Stopping $(basename "$0")..."
-        # mqtt-autodiscovery.sh is not a daemon; it just runs once-and-done. 
+        # mqtt-autodiscovery.sh is not a daemon; it just runs once-and-done.
         kill -s SIGTERM $(pgrep "mosquitto_sub|mqtt-status.sh|mqtt-control.sh|mqtt-motion.sh")
         ;;
     restart)
         $0 stop
         $0 start
         ;;
-	*)
-		echo "Usage: $0 {start|stop|restart}"
-		exit 1
-		;;
+        *)
+                echo "Usage: $0 {start|stop|restart}"
+                exit 1
+                ;;
 esac
 
 
